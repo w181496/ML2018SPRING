1b5424ba4dd69f95f2b8617fce1cbfa9ac0b1c19
w181496 <w181496@gmail.com>
2018-06-10 15:21:43 +0800

    update

--- hw6/hw6.py
+++ hw6/hw6.py
@@ -1,6 +1,3 @@
-import csv
-import numpy as np
-import sys
 from keras import regularizers
 from keras.layers import Input, Embedding, Dense, BatchNormalization, Flatten, dot, add, merge
 from keras.models import Model, load_model
@@ -8,10 +5,13 @@ from keras.callbacks import ModelCheckpoint, EarlyStopping
 from sklearn.model_selection import train_test_split
 from sklearn import  preprocessing
 import pandas as pd
+import csv
+import numpy as np
+import sys
 
 test = pd.read_csv(sys.argv[1], names=["id", "user_id", "movie_id"], header=0)
-movies = pd.read_csv(sys.argv[3], names=['movie_id', 'title', 'genres'], delimiter='::', header=0)
 users = pd.read_csv(sys.argv[4], names=['user_id', 'gender', 'age', 'occupation', 'zip_code'], delimiter='::', header=0)
+movies = pd.read_csv(sys.argv[3], names=['movie_id', 'title', 'genres'], delimiter='::', header=0)
 
 model = load_model('model.h5')
 pred = model.predict([test.user_id, test.movie_id]).squeeze()
